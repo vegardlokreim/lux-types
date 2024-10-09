3940613a4b4d72ff5d4dfe13cf17d8a548dcf032
Vegard Lokreim <vegardlokreim9@gmail.com>
2024-10-09 05:24:18 +0200

    accept any set state actrion type in useFetchDoc

--- src/functions/hooks/useFetchDoc.tsx
+++ src/functions/hooks/useFetchDoc.tsx
@@ -7,26 +7,26 @@ export function useFetchDoc<T>(
     db: Firestore,
     collectionName: FirestoreCollection,
     docId: string | undefined,
-    setData: React.Dispatch<React.SetStateAction<T | undefined>>,
-    setError?: React.Dispatch<React.SetStateAction<string | undefined>> // Error state setter
+    setData: React.Dispatch<React.SetStateAction<any>>,
+    setError?: React.Dispatch<React.SetStateAction<string | undefined>>, // Error state setter
 ) {
-    useEffect(() => {
+    useEffect( () => {
         const fetchDocData = async () => {
             try {
                 if (!docId) return;
-                const docRef = doc(db, collectionName, docId);
-                const docSnapshot = await getDoc(docRef);
+                const docRef = doc( db, collectionName, docId );
+                const docSnapshot = await getDoc( docRef );
 
                 if (docSnapshot.exists()) {
-                    setData(docSnapshot.data() as T); // Casting data to generic type T
+                    setData( docSnapshot.data() as T ); // Casting data to generic type T
                 } else {
-                    setError && setError(`Document with ID ${docId} not found in ${collectionName}`);
+                    setError && setError( `Document with ID ${ docId } not found in ${ collectionName }` );
                 }
             } catch (err) {
-                setError && setError(`Error fetching document: ${err}`);
+                setError && setError( `Error fetching document: ${ err }` );
             }
         };
 
         fetchDocData();
-    }, [docId, collectionName, setData, setError]);
+    }, [ docId, collectionName, setData, setError ] );
 }
