370d5d4504c05fd247d3a3e611917fcdddbb0809
Vegard Lokreim <vegardlokreim9@gmail.com>
2024-10-22 10:55:24 +0200

    minor changes

--- src/types/comonTypes.ts
+++ src/types/comonTypes.ts
@@ -2,12 +2,12 @@ import { firestoreCollections, userStoragePath } from "./backend/consts";
 
 export type Subset<T> = {
     [A in keyof T]?: T[A] extends object
-        ? Subset<T[A]>
-        : T[A] extends object | null
-            ? Subset<T[A]> | null
-            : T[A] extends object | null | undefined
-                ? Subset<T[A]> | null | undefined
-                : T[A]
+    ? Subset<T[A]>
+    : T[A] extends object | null
+    ? Subset<T[A]> | null
+    : T[A] extends object | null | undefined
+    ? Subset<T[A]> | null | undefined
+    : T[A]
 }
 
 export type FirestoreCollection = typeof firestoreCollections[number];
